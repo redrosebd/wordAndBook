38b3a8acf0a37cdffe6fa1c47e9bb120faf6b35a
developer-shourav <mrinmoyraj823@gmail.com>
2024-03-21 20:13:44 +0600

    primary layout complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter, Ubuntu } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar/Navbar";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -16,12 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${ubuntu.className} max-w-[1366px] lg:w-11/12 mx-auto`} >
- 
-        <Navbar/>
         {children}
-         <footer>
-           This is footer
-         </footer>
 
         </body>
  
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import WordAudio from "../app/WordAudio/page";
 import Ebook from "../app/Ebook/page";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main className="">
-       {/* <WordAudio/> */}
-      <Ebook/>
+      <Navbar />
+
+      {/* ------------ ------------ Main Content ------------------------*/}
+      <div className="flex w-12/12 justify-between gap-4 mt-3">
+        {/* -------------Content Menu------------- */}
+        <div className="w-3/12">
+          <h2 className="bg-[#075F8F] font-bold text-[22px] text-white px-2 py-1">Chapters</h2>
+         <ul>
+           <li></li>
+         </ul>
+        </div>
+        {/* -----------Content ---------------- */}
+        <div className="w-9/12">
+          <Ebook />
+        </div>
+
+      </div>
     </main>
   );
 }

f8e8976b7517d58e36f04cbdbf278f718b903102
developer-shourav <mrinmoyraj823@gmail.com>
2024-03-22 13:10:31 +0600

    new custom component added

--- src/app/WordAudio/page.jsx
+++ src/app/WordAudio/page.jsx
@@ -1,36 +1,37 @@
 "use client";
 import { useEffect, useState } from "react";
 import "./style.css";
-import { useSpeechSynthesis } from "react-speech-kit";
 import { MdAudiotrack } from "react-icons/md";
 import { FaHeadset } from "react-icons/fa6";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
-import Link from "next/link";
+import Navbar from "../components/Navbar/Navbar";
 
 function Page() {
   /* ---------- Load All English Words ------------ */
   const [commonWords, setCommonWords] = useState([]);
   useEffect(() => {
     fetch("words.json")
-    fetch("words.json")
       .then((res) => res.json())
       .then((data) => setCommonWords(data));
   }, []);
 
-  console.log(commonWords);
-
   /* -------For Input Field Value------------- */
   const [inputValue, setInputValue] = useState("");
-  const { speak } = useSpeechSynthesis();
+  
+
 
   const handleInputFieldClick = () => {
-    speak({ text: inputValue });
+    const englishText = new SpeechSynthesisUtterance(inputValue);
+    englishText.voice = speechSynthesis.getVoices()[0];
+    speechSynthesis.speak(englishText);
   };
 
   /* -------For English Words form Table Row ------------- */
   const handleWorldClick = (event) => {
     const word = event.target.textContent;
-    speak({ text: word });
+    const englishWord = new SpeechSynthesisUtterance(word);
+    englishWord.voice = speechSynthesis.getVoices()[0];
+    speechSynthesis.speak(englishWord);
   };
 
   return (
@@ -38,49 +39,6 @@ function Page() {
       <Navbar />
 
       <div className=" w-full lg:w-10/12  mx-auto lg:mt-20 lg:py-20 text-center">
-        <div className="my-10">
-          <h1 className="text-center text-lg md:text-2xl lg:text-3xl font-semibold text-gray-700">
-            {commonWords.length} Most Common English Words
-          </h1>
-
-          <div className="overflow-x-auto w-11/12  lg:w-9/12 xl:w-7/12 mx-auto mt-3 md:mt-5 lg:mt-10 mb-20 rounded-md shadow-lg">
-            <table className="table text-[13px] md:text-[16px]">
-              {/*---------- head ------------ */}
-              <thead className="text-lg text-white font-semibold">
-                <tr className="w-full text-[15px] md:text-[16px]">
-                  {/* <th className="bg-blue-400 w-3/12">Rank</th> */}
-                  <th className="bg-purple-400 w-4/12" title="Word With Audio">
-                    {" "}
-                    <span>English</span>{" "}
-                    <HiOutlineSpeakerWave className="inline text-xl md:text-2xl font-bold -mt-1" />
-                  </th>
-                  <th className="bg-orange-400 w-5/12">Bengali Meaning</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {commonWords &&
-                  commonWords.map((item) => {
-                    return (
-                      <tr key={item?.rank}>
-                        {/* <th className="bg-blue-100 ">{word?.rank}</th> */}
-                        <td
-                          title="Click to listen Pronunciation"
-                          onClick={handleWorldClick}
-                          className="bg-blue-100 hover:bg-blue-200 hover:cursor-pointer font-semibold text-gray-700"
-                        >
-                          {item?.word}
-                        </td>
-                        <td className="bg-blue-100 text-gray-700 font-semibold">
-                          {item?.bangla_meaning}
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
-          </div>
-        </div>
         <h1 className=" text-xl md:text-2xl lg:text-4xl font-bold flex justify-center items-center text-gray-700">
           <FaHeadset className="inline text-2xl md:text-3xl lg:text-6xl text-red-500 me-1 lg:me-2" />
           <span>RedRose Audio Book</span>{" "}
@@ -98,7 +56,6 @@ function Page() {
         <br />
 
         <div className="flex justify-center items-center mt-1 md:mt-3 lg:mt-5">
-        <div className="flex justify-center items-center mt-1 md:mt-3 lg:mt-5">
           <button
             onClick={() => {
               handleInputFieldClick();
@@ -109,7 +66,6 @@ function Page() {
             <HiOutlineSpeakerWave className="inline text-xl md:text-2xl font-bold" />{" "}
             Listen{" "}
           </button>
-        </div>
         </div>
       </div>
 
